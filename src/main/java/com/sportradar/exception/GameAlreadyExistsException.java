2ba6ff0ac7acebed2268780316b1559e81757b74
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2025-10-22 00:18:06 +0200

    feat: add Game exceptions

---
+++ src/main/java/com/sportradar/exception/GameAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package com.sportradar.exception;
+
+import java.io.Serial;
+
+public class GameAlreadyExistsException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public GameAlreadyExistsException(String message) {
+        super(message);
+    }
+    public GameAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/sportradar/exception/GameNotFoundException.java
@@ -0,0 +1,16 @@
+package com.sportradar.exception;
+
+import java.io.Serial;
+
+public class GameNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public GameNotFoundException(String message) {
+        super(message);
+    }
+
+    public GameNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
